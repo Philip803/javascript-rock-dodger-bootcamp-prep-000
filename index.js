88dd1225b9e94ce7c90f144c2c2b760984c54fd1
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 04:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ function createRock(x) {
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
+  
   ROCKS.push(rock)
 
   // Finally, return the rock element you've created
