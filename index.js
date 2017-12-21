25466c991d7041ba2b45593bbeafcb40938d426c
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 04:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = dodgerLeftEdge + 40;
+    const dodgerRightEdge = GAME_WIDTH - dodgerLeftEdge - 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = rockLeftEdge + 20;
+    const rockRightEdge = GAME_WIDTH - rockLeftEdge - 20;
     
     if(false
     /**
