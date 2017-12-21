8e2a730fdc9bfa46481f8416ebe8d183a42e5c72
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 04:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,8 +48,7 @@ function checkCollision(rock) {
                *    and the rock's right edge is > the DODGER's right edge
                */
     if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge &&
-    rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
-    || dodgerLeftEdge > 0 || top < 362){       
+    rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge){       
       return true
     } else {
       return false;
