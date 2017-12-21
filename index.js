e3f181e569ae201b5f8eb75af841b9ef852e894d
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 03:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ function createRock(x) {
 function endGame() {
   window.removeEventListener("keydown", moveDodger);
   ROCKS.length = 0;
+  
   clearInterval(gameInterval);
   alert("YOU LOSE!");
 }
