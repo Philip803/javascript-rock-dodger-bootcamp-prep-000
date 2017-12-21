817bf73d254a43e36f4ddb4619e0b160512dcbf3
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 04:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,11 +51,10 @@ function checkCollision(rock) {
       return true
     }
     
-   return (
-      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
-      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
-      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
-    )
+    return 
+      ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge &&
+      rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge))
+    }
 }
 
 function createRock(x) {
